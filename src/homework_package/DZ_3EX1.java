5352a0950d356e270ad7f2317065056aed740a59
AntonVerovkin <anton.veryovkin@gmail.com>
2023-01-02 17:56:34 +0100

    initial commit

---
+++ src/homework_package/DZ_3EX1.java
@@ -0,0 +1,18 @@
+package homework_package;
+
+public class DZ_3EX1 {
+    public static void main(String[] args) {
+        int[] arrayInt = {
+                1,
+                2,
+                3,
+                4,
+                5
+        };
+        System.out.println(arrayInt[0]);
+        System.out.println(arrayInt[1]);
+        System.out.println(arrayInt[2]);
+        System.out.println(arrayInt[3]);
+        System.out.println(arrayInt[4]);
+    }
+}
---
+++ src/homework_package/DZ_3EX2.java
@@ -0,0 +1,21 @@
+package homework_package;
+
+public class DZ_3EX2 {
+    public static void main(String[] args) {
+        int[] arrayInt = {
+                1,
+                2,
+                3,
+                4,
+                5
+        };
+        System.out.println(arrayInt[0] *= arrayInt[0]);
+        System.out.println(arrayInt[1] *= arrayInt[1]);
+        System.out.println(arrayInt[2] *= arrayInt[2]);
+        System.out.println(arrayInt[3] *= arrayInt[3]);
+        System.out.println(arrayInt[4] *= arrayInt[4]);
+
+
+
+    }
+}
