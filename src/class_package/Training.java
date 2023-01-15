0922520657f1fc43d8a57839ff7426122f33469f
AntonVerovkin <anton.veryovkin@gmail.com>
2023-01-15 17:42:00 +0100

    initial commit

---
+++ src/class_package/Training.java
@@ -0,0 +1,42 @@
+package class_package;
+
+import java.util.Scanner;
+
+public class Training {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the Celsius: ");
+//        int userCelsius = scanner.nextInt();
+//        System.out.println(userCelsius +" "+ "C"+ " " + "="+ " "+ converterFromCelsiusToFahranheit(userCelsius)+ " " + "F");
+//
+//        System.out.println(userCelsius +" "+ "C"+ " " + "="+ " "+ converterFromCelsiusToKelvin(userCelsius)+ " " + "K");
+        System.out.println("Enter the first number: ");
+        int userNum1 = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int userNum2 = scanner.nextInt();
+        System.out.println(
+                "Число" + " " + compareMagnitudeOfNumbers(userNum1, userNum2)
+                        + " " + "оказалось"
+                        + " " + "больше.");
+    }
+
+    public static int converterFromCelsiusToFahranheit(int celsius) {
+        return celsius * 9 / 5 + 32;
+    }
+
+    public static double converterFromCelsiusToKelvin(int celsius) {
+        return celsius + 273;
+    }
+
+    public static int compareMagnitudeOfNumbers(int num1, int num2) {
+        /*if (Math.abs(num1) > Math.abs(num2)){
+            return num1;
+        }else {
+            return num2;
+        }*/
+
+        return Math.abs(num1) > Math.abs(num2) ? num1 : num2;
+
+
+    }
+}
